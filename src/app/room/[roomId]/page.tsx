1d01c3ff83ea499b0c6a0c35c3492316c80a5f1b
User <fistkwt@gmail.com>
2025-04-03 15:22:08 +0300

    updating: the game  000

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -190,7 +190,7 @@ export default function RoomPage({ params }: { params: Promise<{ roomId: string 
             ? 'شرطي'
             : role === 'sniper'
             ? 'قناص'
-            : 'طبيب'}
+            : 'زق'}
         </div>
       )}
     </main>
